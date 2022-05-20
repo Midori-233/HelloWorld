1458c6a8bc22756a0614fde55bcef4b1fe87262d
Midori <midori233@qq.com>
2022-05-20 09:59:35 +0800

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-// 设置图片切换
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -10,12 +9,9 @@ myImage.onclick = function() {
   }
 };
 
-// 设置个性化欢迎信息
-// 获取新按钮和标题的引用
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
-// 个性化欢迎信息设置函数
 function setUserName() {
   let myName = prompt('请输入你的名字。');
   if (!myName || myName === null) {
@@ -26,7 +22,6 @@ function setUserName() {
   }
 }
 
-// 初始化代码：在页面初次读取时进行构造工作：
 if (!localStorage.getItem('name')) {
   setUserName();
 } else {
@@ -34,7 +29,6 @@ if (!localStorage.getItem('name')) {
   myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
 }
 
-// 为按钮设置 onclick 事件处理器：
 myButton.onclick = function() {
   setUserName();
 };
